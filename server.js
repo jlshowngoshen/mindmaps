fcc721511dd8c6c0ffbb90a29013b6876422f83c
littleguga <fed777os@gmail.com>
2017-07-01 01:35:12 +0300

    fix markup

--- server.js
+++ server.js
@@ -6,23 +6,23 @@ var path = 'src';
 
 var args = process.argv;
 if (args.length >= 3 && args[2] == '--production') {
-  path = 'bin';
+    path = 'bin';
 }
 
-var file = new(static.Server)(path, { cache: false });
+var file = new (static.Server)(path, {cache: false});
 
 require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    file.serve(request, response, function (err, res) {
-      if (err) {
-        sys.error("> Error serving " + request.url + " - " + err.message);
-        response.writeHead(err.status, err.headers);
-        response.end();
-      } else {
-        sys.puts("> " + request.url + " - " + res.message);
-      }
+    request.addListener('end', function () {
+        file.serve(request, response, function (err, res) {
+            if (err) {
+                sys.error("> Error serving " + request.url + " - " + err.message);
+                response.writeHead(err.status, err.headers);
+                response.end();
+            } else {
+                sys.puts("> " + request.url + " - " + res.message);
+            }
+        });
     });
-  });
 }).listen(8080);
 
 sys.puts("> node-static is listening on http://127.0.0.1:8080");
